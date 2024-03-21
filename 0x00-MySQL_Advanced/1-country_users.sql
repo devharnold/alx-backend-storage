bd7b345e94ae40c7627c37c3a67679bb79e7a60d
Henry Arnold <devharnold@gmail.com>
2024-03-21 20:41:11 +0000

    user by country

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -5,7 +5,7 @@
 -- `country` enumeration (US, CO, TN)never null
 -- if table exists, script should not fail
 
-CREATE TABLE IF NOT EXISTS user (
+CREATE TABLE IF NOT EXISTS users (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	email VARCHAR(255) NOT NULL UNIQUE,
 	name VARCHAR(255)
