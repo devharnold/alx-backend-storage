66024877ceb2d60b59ddce7d5ea94f5ba48dd3c9
Henry Arnold <devharnold@gmail.com>
2024-03-21 20:37:55 +0000

    add with enum of countries

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,13 @@
+-- Table `users`
+-- `id` int, never null, auto increment, and primary key
+-- `email` string(255), never null and unique
+-- `name` string(255)
+-- `country` enumeration (US, CO, TN)never null
+-- if table exists, script should not fail
+
+CREATE TABLE IF NOT EXISTS user (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255)
+	country ENUM(US CO TN) NOT NULL
+);
