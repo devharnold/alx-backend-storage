ae301bf8ac945118ab22f77ba47f5f7700afc5cc
Henry Arnold <devharnold@gmail.com>
2024-04-01 12:49:23 +0300

    try and modify

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@ Writing strings to redis
 '''
 import redis
 import uuid
-import sys
+from typing import Union, Callable
 
 
 class Cache():
@@ -12,17 +12,28 @@ class Cache():
         '''Constructor for __init__ method'''
         self._redis = redis.Redis(host='localhost', port=6379, db=0)
 
-    def store(self, data: str) -> str:
-        '''A store method that takes a string and returns a unique identifier'''
-        key = str(uuid.uuid4())
-        self._redis.set(key, data)
+    def store(self, value: Union[str, bytes, int]) -> str:
+            key = self.generate_key()
+            self.redis_conn.set(key, value)
+            return key
 
-        return key
+#    def store(self, data: str) -> str:
+#        '''A store method that takes a string and returns a unique identifier'''
+#        key = str(uuid.uuid4())
+#        self._redis.set(key, data)
+#
+#        return key
     
-    def get_str(self, key: str) -> str:
-        '''A get method that takes a key and returns the string stored in the key'''
-        return self._redis.get(key)
-    
-    def get_int(self, key: int) -> int:
-        '''A get method that takes a key and returns the integer stored in the key'''
-        return self._redis.get(key)
+def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, None]:
+    value = self.reddis_conn.get(key)
+    if value is None:
+        return None
+    if fn is not None:
+        return fn(value)
+    return value
+
+def get_str(self, key: str) -> Union[str, bytes, None]:
+    return self.get(key, fn=lambda x: x.decode('utf-8'))
+
+def get_int(self, key: str) -> Union[int, bytes, None]:
+    return self.get(key, fn=int)
